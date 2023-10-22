76f1fc8272f9a6ee1129fb38b44a5025b82a45bc
Lilanga <49157841+geekAmis@users.noreply.github.com>
2023-10-22 19:13:57 +0300

    Add files via upload

--- grr.py
+++ grr.py
@@ -4,31 +4,24 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/api_filesIn')
-def api_get_files():
-	return jsonify([{
-		"filename":i,
-		"size":os.stat(i).st_size if os.path.isfile(i) else len(os.listdir(i))} for i in os.listdir()
-	])
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def main_page():
+	if request.form.get("val"):
+		try:
+			os.system("pip install {}".format(
+				request.form.get("val")
+			))
+			text = "Download if y"
+		except:
+			pass
+
+		return render_template("index.html",desc=text)
+
 	return render_template("index.html")
 
 
 
-@app.route('/size/<filename>/')
-def filename_sized(filename):
-	return """
-	{} - <h1 class="ok"></h1>
-
-	<script src="/static/dkfgnd.js">
-	</script>
-	<script>
-	get("{}");
-	</script>
-	""".format(filename,filename)
-
 if __name__ == '__main__':
 	app.debug = True
-	app.run(host="127.0.0.1",port = 80)
+	app.run(host="localhost",port=80)
